f8b5b32023e1d975b9f592d04303e77ae64f1b95
Lailz <lailaabdulraheem@gmail.com>
2022-04-20 06:35:15 +0300

    register project and semester models in admin site

--- team_app/admin.py
+++ team_app/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+
+from team_app.models import Project, Semester
+
+admin.site.register(Semester)
+admin.site.register(Project)
--- team_app/models.py
+++ team_app/models.py
@@ -7,6 +7,9 @@ class Semester(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
@@ -17,6 +20,9 @@ class Project(models.Model):
     slug = models.SlugField()
     semester = models.ForeignKey(
         Semester, related_name="projects", on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
